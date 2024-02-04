113dda08baeda525dd31569f8d48a5a7902e61b6
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-02-04 11:58:12 -0500

    Added sharing, updated links.

--- script.js
+++ script.js
@@ -16,18 +16,31 @@ var sfxLeft = new Audio("./Images/Sounds/2_l.mp3");
 var sfxRight = new Audio("./Images/Sounds/3_R.mp3");
 var sfxUp = new Audio("./Images/Sounds/4_U.mp3");
 
-let currentStratagemDone = true;
-let currentSequenceIndex = 0;
+// Create global tracking variables
+var gameIsRunning = true;
+const NEW_STRATEGEM_TIMEOUT = 200;
+var currentStratagem = undefined;
+var currentStratagemDone = true;
+var currentSequenceIndex = 0;
 var currentArrowSequenceTags = undefined;
+const TOTAL_TIME = 5000;
+const COUNTDOWN_STEP = 10;
+const CORRECT_TIME_BONUS = 500;
+var timeRemaining = TOTAL_TIME;
+var numCompletedStratagems = 0
+var completedStrategemsList = [];
 
 // Load first stratagem
 loadNextStratagem();
+
+// Bootstrap countdown timer
+countDown();
 
 //~~~//
 
 function keypress(keyCode){
     // Guard clause; don't do anything if there's no active stratagem
-    if(currentStratagemDone)
+    if(currentStratagemDone || !gameIsRunning)
         return;
 
     // Ignore invalid keypresses
@@ -47,7 +60,6 @@ function keypress(keyCode){
             sfx = sfxRight;
             break;
         default: 
-            console.log("Ignored Key");
             return;
     }
 
@@ -55,32 +67,29 @@ function keypress(keyCode){
     sfx.paused ? sfx.play() : sfx.currentTime = 0;
 
     // Check the keypress against the current sequence
-    console.log(`Checking ${keyCode} against ${currentArrowSequenceTags[currentSequenceIndex].code}`);
     if(keyCode == currentArrowSequenceTags[currentSequenceIndex].code){
         //Success, apply the success
-        //TODO: Set currentArrowSequenceTags[currentSequenceIndex] to completed color
         currentSequenceIndex++;
-        console.log(currentSequenceIndex);
         
         //Check if that success completes the entire sequence. 
         if(currentSequenceIndex == currentArrowSequenceTags.length){
-            console.log("Stratagem done, loading next");
-            currentSequenceIndex = 0;
-            loadNextStratagem();
+            timeRemaining += CORRECT_TIME_BONUS;
+            completedStrategemsList.push(currentStratagem);
+            setTimeout(() => {
+                currentSequenceIndex = 0;
+                loadNextStratagem();
+            }, NEW_STRATEGEM_TIMEOUT);
         }
     }
     else if (keyCode == currentArrowSequenceTags[0].code){
         //Edge case; if they're wrong but their input is the same as the first code, reset to first.
-        //TODO: Set all currentArrowSequenceTags to normal color and play some kind of failure animation
-        //TODO: Set currentArrowSequenceTags[0] to completed color
+        //TODO: play some kind of failure animation
         currentSequenceIndex = 1;
-        console.log("Fail but maintaining first correct input.");
     }
     else{
         //Failure, reset progress
-        //TODO: Set all currentArrowSequenceTags to normal color and play some kind of failure animation
+        //TODO: play some kind of failure animation
         currentSequenceIndex = 0;
-        console.log("Fail!");
     }
 
     updateArrowFilters();
@@ -94,7 +103,7 @@ function updateArrowFilters(){
 
 function loadNextStratagem(){
     // Pick a random stratagem
-    let currentStratagem = pickRandomStratagem();
+    currentStratagem = pickRandomStratagem();
     // console.log(currentStratagem);
 
     // Set the stratagem's picture
@@ -147,6 +156,95 @@ function showArrowSequence(arrowSequence){
         arrowTags.push(img);
     }
     return arrowTags;
+}
+
+function gameOver(){
+    //Stop the game
+    gameIsRunning = false;
+
+    // Write score to readout
+    let scoreReadout = document.getElementById("score-readout");
+    scoreReadout.innerHTML = `SCORE: ${numCompletedStratagems}`
+
+    // Write completed strategems to readout
+    let stratagemReadout = document.getElementById("completed-strategems-readout");
+    stratagemReadout.innerHTML = stratagemListToString(true);
+
+    // Show the popup
+    let popup = document.getElementById("game-over-popup");
+    popup.removeAttribute("hidden");
+    popup.style.visibility = "visible";
+}
+
+function stratagemListToString(html){
+    let re = "";
+    for(let stratagem of completedStrategemsList){
+        re += stratagem.name + ": \t\t";
+        for(let arrow of stratagem.sequence){
+            switch(arrow){
+                case "Arrow_4_U.png":
+                    re += "🡅";
+                break;
+                case "Arrow_1_D.png":
+                    re += "🡇";
+                break;
+                case "Arrow_2_L.png":
+                    re += "🡄";
+                break;
+                case "Arrow_3_R.png":
+                    re += "🡆";
+                break;
+            }
+        }
+        re += html ? "<br>" : "\n";
+    }
+
+    return re;
+}
+
+function copyShare(){
+    // Gather text and write to clipboard
+    let output = `My Strategem Hero Online Score: ${completedStrategemsList.length}\n`
+    output += stratagemListToString(false);
+    output += "Do your part! Try Strategem Hero Online here: https://combustibletoast.github.io/"
+    navigator.clipboard.writeText(output);
+
+    //Change button's text
+    let buttonElement = document.getElementById("share-button");
+    let buttonOriginalText = buttonElement.innerHTML;
+    buttonElement.innerHTML = "Copied!";
+
+    //Set timeout to change it back
+    //Doesn't work, unable to pass in original text
+    setTimeout((buttonElement, buttonOriginalText) => {
+        buttonElement.innerHTML = buttonOriginalText;
+    }, 3000);
+}
+
+async function countDown(){
+    if(!gameIsRunning)
+        return;
+
+    if(timeRemaining <= 0){
+        gameOver();
+    }
+
+    // Apply countdown
+    timeRemaining -= COUNTDOWN_STEP;
+    updateTimeBar();
+
+    // Set timeout for next countdown step
+    setTimeout(() => {
+        countDown();
+        // console.log(timeRemaining)
+    }, COUNTDOWN_STEP);
+}
+
+function updateTimeBar(){
+    let bar = document.getElementById("time-remaining-bar");  
+    let width = (timeRemaining/TOTAL_TIME) * 100;
+    // console.log(width);
+    bar.style.width = `${width}%`;   
 }
 
 async function sleep(ms){

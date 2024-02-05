1f61a8f9453328057a98f96fae89268efca7620d
Ena Chu <combustibletoast@gmail.com>
2024-02-04 20:55:17 -0500

    Updated refresh combination to reinforce

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ function gameOver(){
     stratagemReadout.innerHTML = stratagemListToString(true);
 
     // Show refresh arrow sequence
-    let sequence = ["Arrow_2_L.png", "Arrow_4_U.png", "Arrow_3_R.png"];
+    let sequence = ["Arrow_4_U.png", "Arrow_1_D.png", "Arrow_3_R.png", "Arrow_2_L.png", "Arrow_4_U.png"];
     let container = document.getElementById("refresh-arrows-container");
     refreshArrowSequenceTags = showArrowSequence(sequence, container);
     // console.log(`Refresh tags ${refreshArrowSequenceTags}`);

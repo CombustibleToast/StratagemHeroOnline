c11bbbfc82065a4170bec744006c9b2947dbe549
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-02-06 10:29:30 -0500

    removed usless script oops

--- pushToProd.bat
+++
@@ -1,5 +0,0 @@
-git clone git@github.com:CombustibleToast/StratagemHeroOnline.git
-cd StratagemHeroOnline
-git remote set-url origin git@github.com:CombustibleToast/combustibletoast.github.io.git
-git pull
-git push
